751c946e5e252fa044b8ec83ac54727b73b2df54
Michael Samuel <mik@miknet.net>
2013-06-07 21:55:04 +1000

    de-dup live

--- pwgenphonemes.py
+++ pwgenphonemes.py
@@ -12,6 +12,9 @@ TODO: Merge duplicate branches
 A full run with 6 chars produced 1124226450 possibilities
 """ 
 
+from __future__ import print_function
+from __future__ import division
+
 import operator
 import numpy
 import sys
@@ -27,73 +30,71 @@ NOT_FIRST = 8
 NUMBER=16
 
 class CharsetItem(tuple):
-	def __new__(cls, c, flags, weight):
-		return tuple.__new__(cls, (c, flags, numpy.float128(weight)))
+	def __new__(cls, c, flags):
+		return tuple.__new__(cls, (c, flags))
 
 	c = property(operator.itemgetter(0))
 	flags = property(operator.itemgetter(1))
-	weight = property(operator.itemgetter(2))
 	
 charset = [
-	CharsetItem("a", VOWEL, 2.0/64),
-	CharsetItem("ae", VOWEL | DIPTHONG, 2.0/64),
-	CharsetItem("ah", VOWEL | DIPTHONG, 2.0/64),
-	CharsetItem("ai", VOWEL | DIPTHONG, 2.0/64),
-	CharsetItem("b", CONSONANT, 2.0/64),
-	CharsetItem("c", CONSONANT, 2.0/64),
-	CharsetItem("ch", CONSONANT | DIPTHONG, 2.0/64),
-	CharsetItem("d", CONSONANT, 2.0/64),
-	CharsetItem("e", VOWEL, 2.0/64),
-	CharsetItem("ee", VOWEL | DIPTHONG, 2.0/64),
-	CharsetItem("ei", VOWEL | DIPTHONG, 2.0/64),
-	CharsetItem("f", CONSONANT, 2.0/64),
-	CharsetItem("g", CONSONANT, 2.0/64),
-	CharsetItem("gh", CONSONANT | DIPTHONG | NOT_FIRST, 2.0/64),
-	CharsetItem("h", CONSONANT, 2.0/64),
-	CharsetItem("i", VOWEL, 2.0/64),
-	CharsetItem("ie", VOWEL | DIPTHONG, 2.0/64),
-	CharsetItem("j", CONSONANT, 2.0/64),
-	CharsetItem("k", CONSONANT, 2.0/64),
-	CharsetItem("l", CONSONANT, 2.0/64),
-	CharsetItem("m", CONSONANT, 2.0/64),
-	CharsetItem("n", CONSONANT, 2.0/64),
-	CharsetItem("ng", CONSONANT | DIPTHONG | NOT_FIRST, 2.0/64),
-	CharsetItem("o", VOWEL, 2.0/64),
-	CharsetItem("oh", VOWEL | DIPTHONG, 1.0/64),
-	CharsetItem("oo", VOWEL | DIPTHONG, 1.0/64),
-	CharsetItem("p", CONSONANT, 1.0/64),
-	CharsetItem("ph", CONSONANT | DIPTHONG, 1.0/64),
-	CharsetItem("qu", CONSONANT | DIPTHONG, 1.0/64),
-	CharsetItem("r", CONSONANT, 1.0/64),
-	CharsetItem("s", CONSONANT, 1.0/64),
-	CharsetItem("sh", CONSONANT | DIPTHONG, 1.0/64),
-	CharsetItem("t", CONSONANT, 1.0/64),
-	CharsetItem("th", CONSONANT | DIPTHONG, 1.0/64),
-	CharsetItem("u", VOWEL, 1.0/64),
-	CharsetItem("v", CONSONANT, 1.0/64),
-	CharsetItem("w", CONSONANT, 1.0/64),
-	CharsetItem("x", CONSONANT, 1.0/64),
-	CharsetItem("y", CONSONANT, 1.0/64),
-	CharsetItem("z", CONSONANT, 1.0/64),
+	CharsetItem("a", VOWEL),
+	CharsetItem("ae", VOWEL | DIPTHONG),
+	CharsetItem("ah", VOWEL | DIPTHONG),
+	CharsetItem("ai", VOWEL | DIPTHONG),
+	CharsetItem("b", CONSONANT),
+	CharsetItem("c", CONSONANT),
+	CharsetItem("ch", CONSONANT | DIPTHONG),
+	CharsetItem("d", CONSONANT),
+	CharsetItem("e", VOWEL),
+	CharsetItem("ee", VOWEL | DIPTHONG),
+	CharsetItem("ei", VOWEL | DIPTHONG),
+	CharsetItem("f", CONSONANT),
+	CharsetItem("g", CONSONANT),
+	CharsetItem("gh", CONSONANT | DIPTHONG | NOT_FIRST),
+	CharsetItem("h", CONSONANT),
+	CharsetItem("i", VOWEL),
+	CharsetItem("ie", VOWEL | DIPTHONG),
+	CharsetItem("j", CONSONANT),
+	CharsetItem("k", CONSONANT),
+	CharsetItem("l", CONSONANT),
+	CharsetItem("m", CONSONANT),
+	CharsetItem("n", CONSONANT),
+	CharsetItem("ng", CONSONANT | DIPTHONG | NOT_FIRST),
+	CharsetItem("o", VOWEL),
+	CharsetItem("oh", VOWEL | DIPTHONG),
+	CharsetItem("oo", VOWEL | DIPTHONG),
+	CharsetItem("p", CONSONANT),
+	CharsetItem("ph", CONSONANT | DIPTHONG),
+	CharsetItem("qu", CONSONANT | DIPTHONG),
+	CharsetItem("r", CONSONANT),
+	CharsetItem("s", CONSONANT),
+	CharsetItem("sh", CONSONANT | DIPTHONG),
+	CharsetItem("t", CONSONANT),
+	CharsetItem("th", CONSONANT | DIPTHONG),
+	CharsetItem("u", VOWEL),
+	CharsetItem("v", CONSONANT),
+	CharsetItem("w", CONSONANT),
+	CharsetItem("x", CONSONANT),
+	CharsetItem("y", CONSONANT),
+	CharsetItem("z", CONSONANT),
 ]
-charset.extend([CharsetItem(str(x), NUMBER, 2.0/16) for x in range(0, 6)])
-charset.extend([CharsetItem(str(x), NUMBER, 1.0/16) for x in range(6, 10)])
+charset.extend([CharsetItem(str(x), NUMBER) for x in range(0, 10)])
 
 class Possibility(object):
 	def __init__(self, flags, weight, next_state, upper=False):
 		self.flags = flags
+		self.items = [ item for item in charset if item.flags == self.flags ]
 		self.weight = numpy.float128(weight)
+		self.itemWeight = self.weight / len(self.items)
 		self.next_state = next_state
 		self.upper = upper
 
 	def __iter__(self):
-		items = [ item for item in charset if item.flags == self.flags ]
-		wFactor = 1.0 / sum([ item.weight for item in items ])
-		for item in items:
+		for item in self.items:
 			c = item.c
 			if self.upper:
 				c = c.capitalize()
-			yield (c, item.flags, item.weight*wFactor*self.weight)
+			yield (c, item.flags, self.itemWeight)
 		
 	@property
 	def numchars(self):
@@ -102,19 +103,35 @@ class Possibility(object):
 		else:
 			return 1
 
-def uniqify(iterators):
-	it = heapq.merge(*iterators)
+def merge_and_sort(iterators):
+	ilist = []
+	for it in iterators:
+		try:
+			ilist.append((next(it), it))
+		except StopIteration:
+			pass
 
-	(lastItem, p) = next(it)
+	while len(ilist) > 0:
+		ilist.sort(key=operator.itemgetter(0))
+		(item, it) = ilist.pop(-1)
+		yield item
+		try:
+			ilist.append((next(it), it))
+		except StopIteration:
+			pass
+
+def uniqify(iterators):
+	it = merge_and_sort(iterators)
+
+	lastItem = next(it)
 
 	for item in it:
-		if item[0] == lastItem:
-			p += item[1]
+		if item.password == lastItem.password:
+			lastItem += item
 		else:
-			if lastItem is not None:
-				yield (lastItem, p)
-			lastItem = item[0]
-			p = item[1]
+			yield lastItem
+			lastItem = item
+	yield lastItem
 
 class Result(tuple):
 	def __new__(cls, password, probability):
@@ -123,40 +140,29 @@ class Result(tuple):
 	password = property(operator.itemgetter(0))
 	probability = property(operator.itemgetter(1))
 
-	def __lt__(self, item):
-		# FIXME: Make the sort order more awesome
-		return self.password < item.password
-
-	def __gt__(self, item):
-		# FIXME: Make the sort order more awesome
-		return self.password > item.password
-
-	def __eq__(self, item):
-		return self.password == item.password
-
-	def __ne__(self, item):
-		return self.password != item.password
-
-
+	def __add__(self, other):
+		assert(self.password == other.password)
+		return self.__class__(self.password, self.probability + other.probability)
 
 class State(object):
-	def __init__(self, sofar="", probability=1.0, generated_upper=False, generated_number=False):
-		self.sofar = sofar
-		self.probability = numpy.float128(probability)
-		self.generated_upper = generated_upper
-		self.generated_number = generated_number
-
-	def __iter__(self):
-		if len(self.sofar) == PASSWORD_LENGTH:
-			if self.generated_upper and self.generated_number:
-				yield Result(self.sofar, self.probability)
+	def generate(self, sofar="", probability=1.0, generated_upper=False, generated_number=False):
+		if len(sofar) == PASSWORD_LENGTH:
+			if generated_upper and generated_number:
+				yield Result(sofar, probability)
 			return
-		if len(self.sofar) > PASSWORD_LENGTH:
+		if len(sofar) > PASSWORD_LENGTH:
 			return
 
-		for possibility in self.possibilities:
-			children = []
-			children = [ iter(possibility.next_state(self.sofar+c, self.probability * weight, (self.generated_upper or possibility.upper), (self.generated_number or flags == NUMBER))) for (c, flags, weight) in iter(possibility) ]
+		for pg in self.possibilities:
+			children = [	possibility.next_state().generate(
+						sofar+c,
+						probability * weight,
+						(generated_upper or possibility.upper),
+						(generated_number or flags == NUMBER)
+					)
+				for possibility in pg
+				for (c, flags, weight) in iter(possibility)
+			]
 			yield from uniqify(children)
 
 	def combinations(self, combinations=1, length=0, haveUpper=False, haveNumber=False):
@@ -170,10 +176,11 @@ class State(object):
 			yield 0
 			return
 
-		for p in self.possibilities:
-			c = len(list(iter(p)))
-			s = p.next_state("x" * length)
-			yield sum(s.combinations(c, length + p.numchars, haveUpper or p.upper, haveNumber or p.flags == NUMBER)) * combinations
+		for pg in self.possibilities:
+			for p in pg:
+				c = len(list(iter(p)))
+				s = p.next_state()
+				yield sum(s.combinations(c, length + p.numchars, haveUpper or p.upper, haveNumber or p.flags == NUMBER)) * combinations
 			
 
 def joint_weight(flag, *others):
@@ -200,62 +207,81 @@ class s_after_double_vowel(State):
 	pass
 
 s_first.possibilities = [
-	Possibility(VOWEL|DIPTHONG,	joint_weight(VOWEL|DIPTHONG, VOWEL) * 0.5 * 0.75, s_after_double_vowel),
-	Possibility(VOWEL,		joint_weight(VOWEL, VOWEL|DIPTHONG) * 0.5 * 0.75, s_after_vowel),
-	Possibility(VOWEL|DIPTHONG,	joint_weight(VOWEL|DIPTHONG, VOWEL) * 0.5 * 0.25, s_after_double_vowel, True),
-	Possibility(VOWEL,		joint_weight(VOWEL, VOWEL|DIPTHONG) * 0.5 * 0.25, s_after_vowel, True),
-	Possibility(CONSONANT|DIPTHONG,	joint_weight(CONSONANT|DIPTHONG, CONSONANT) * 0.5 * 0.75, s_after_consonant),
-	Possibility(CONSONANT,		joint_weight(CONSONANT, CONSONANT|DIPTHONG) * 0.5 * 0.75, s_after_consonant),
-	Possibility(CONSONANT|DIPTHONG,	joint_weight(CONSONANT|DIPTHONG, CONSONANT) * 0.5 * 0.25, s_after_consonant, True),
-	Possibility(CONSONANT,		joint_weight(CONSONANT, CONSONANT|DIPTHONG) * 0.5 * 0.25, s_after_consonant, True),
+	[
+		Possibility(VOWEL|DIPTHONG,	joint_weight(VOWEL|DIPTHONG, VOWEL) * 0.5 * 0.75, s_after_double_vowel),
+		Possibility(VOWEL,		joint_weight(VOWEL, VOWEL|DIPTHONG) * 0.5 * 0.75, s_after_vowel),
+	],
+	[
+		Possibility(VOWEL|DIPTHONG,	joint_weight(VOWEL|DIPTHONG, VOWEL) * 0.5 * 0.25, s_after_double_vowel, True),
+		Possibility(VOWEL,		joint_weight(VOWEL, VOWEL|DIPTHONG) * 0.5 * 0.25, s_after_vowel, True),
+	],
+	[
+		Possibility(CONSONANT|DIPTHONG,	joint_weight(CONSONANT|DIPTHONG, CONSONANT) * 0.5 * 0.75, s_after_consonant),
+		Possibility(CONSONANT,		joint_weight(CONSONANT, CONSONANT|DIPTHONG) * 0.5 * 0.75, s_after_consonant),
+	],
+	[
+		Possibility(CONSONANT|DIPTHONG,	joint_weight(CONSONANT|DIPTHONG, CONSONANT) * 0.5 * 0.25, s_after_consonant, True),
+		Possibility(CONSONANT,		joint_weight(CONSONANT, CONSONANT|DIPTHONG) * 0.5 * 0.25, s_after_consonant, True),
+	],
 ]
 
 s_after_consonant.possibilities = [
-	Possibility(VOWEL|DIPTHONG,	joint_weight(VOWEL|DIPTHONG, VOWEL) * 0.625, s_after_double_vowel),
-	Possibility(VOWEL,		joint_weight(VOWEL, VOWEL|DIPTHONG) * 0.625, s_after_vowel),
-	Possibility(NUMBER,		0.375, s_first),
+	[	Possibility(NUMBER,		0.375, s_first), ],
+	[
+		Possibility(VOWEL|DIPTHONG,	joint_weight(VOWEL|DIPTHONG, VOWEL) * 0.625, s_after_double_vowel),
+		Possibility(VOWEL,		joint_weight(VOWEL, VOWEL|DIPTHONG) * 0.625, s_after_vowel),
+	],
 ]
 
 s_after_vowel.possibilities = [
-	Possibility(VOWEL, 0.625 * 0.25, s_after_double_vowel),
-	Possibility(NUMBER, 0.375, s_first),
-	Possibility(CONSONANT|DIPTHONG, 		joint_weight(CONSONANT|DIPTHONG, CONSONANT, CONSONANT|DIPTHONG|NOT_FIRST) * 0.625 * 0.75 * 0.75, s_after_consonant),
-	Possibility(CONSONANT,				joint_weight(CONSONANT, CONSONANT|DIPTHONG, CONSONANT|DIPTHONG|NOT_FIRST) * 0.625 * 0.75 * 0.75, s_after_consonant),
-	Possibility(CONSONANT|DIPTHONG|NOT_FIRST, 	joint_weight(CONSONANT|DIPTHONG|NOT_FIRST, CONSONANT|DIPTHONG, CONSONANT) * 0.625 * 0.75 * 0.75, s_after_consonant),
-	Possibility(CONSONANT|DIPTHONG, 		joint_weight(CONSONANT|DIPTHONG, CONSONANT, CONSONANT|DIPTHONG|NOT_FIRST) * 0.625 * 0.75 * 0.25, s_after_consonant, True),
-	Possibility(CONSONANT,				joint_weight(CONSONANT, CONSONANT|DIPTHONG, CONSONANT|DIPTHONG|NOT_FIRST) * 0.625 * 0.75 * 0.25, s_after_consonant, True),
-	Possibility(CONSONANT|DIPTHONG|NOT_FIRST, 	joint_weight(CONSONANT|DIPTHONG|NOT_FIRST, CONSONANT|DIPTHONG, CONSONANT) * 0.625 * 0.75 * 0.25, s_after_consonant, True),
+	[	Possibility(VOWEL, 0.625 * 0.25, s_after_double_vowel), ],
+	[	Possibility(NUMBER, 0.375, s_first), ],
+	[
+		Possibility(CONSONANT|DIPTHONG, 		joint_weight(CONSONANT|DIPTHONG, CONSONANT, CONSONANT|DIPTHONG|NOT_FIRST) * 0.625 * 0.75 * 0.75, s_after_consonant),
+		Possibility(CONSONANT,				joint_weight(CONSONANT, CONSONANT|DIPTHONG, CONSONANT|DIPTHONG|NOT_FIRST) * 0.625 * 0.75 * 0.75, s_after_consonant),
+		Possibility(CONSONANT|DIPTHONG|NOT_FIRST, 	joint_weight(CONSONANT|DIPTHONG|NOT_FIRST, CONSONANT|DIPTHONG, CONSONANT) * 0.625 * 0.75 * 0.75, s_after_consonant),
+	],
+	[
+		Possibility(CONSONANT|DIPTHONG, 		joint_weight(CONSONANT|DIPTHONG, CONSONANT, CONSONANT|DIPTHONG|NOT_FIRST) * 0.625 * 0.75 * 0.25, s_after_consonant, True),
+		Possibility(CONSONANT,				joint_weight(CONSONANT, CONSONANT|DIPTHONG, CONSONANT|DIPTHONG|NOT_FIRST) * 0.625 * 0.75 * 0.25, s_after_consonant, True),
+		Possibility(CONSONANT|DIPTHONG|NOT_FIRST, 	joint_weight(CONSONANT|DIPTHONG|NOT_FIRST, CONSONANT|DIPTHONG, CONSONANT) * 0.625 * 0.75 * 0.25, s_after_consonant, True),
+	],
 ]
 
 s_after_double_vowel.possibilities = [
-	Possibility(NUMBER, 0.375, s_first),
-	Possibility(CONSONANT,				joint_weight(CONSONANT, CONSONANT|DIPTHONG, CONSONANT|DIPTHONG|NOT_FIRST) * 0.625 * 0.75, s_after_consonant),
-	Possibility(CONSONANT|DIPTHONG,			joint_weight(CONSONANT|DIPTHONG, CONSONANT, CONSONANT|DIPTHONG|NOT_FIRST) * 0.625 * 0.75, s_after_consonant),
-	Possibility(CONSONANT|DIPTHONG|NOT_FIRST,	joint_weight(CONSONANT|DIPTHONG|NOT_FIRST, CONSONANT, CONSONANT|DIPTHONG) * 0.625 * 0.75, s_after_consonant),
-	Possibility(CONSONANT,				joint_weight(CONSONANT, CONSONANT|DIPTHONG, CONSONANT|DIPTHONG|NOT_FIRST) * 0.625 * 0.25, s_after_consonant, True),
-	Possibility(CONSONANT|DIPTHONG,			joint_weight(CONSONANT|DIPTHONG, CONSONANT, CONSONANT|DIPTHONG|NOT_FIRST) * 0.625 * 0.25, s_after_consonant, True),
-	Possibility(CONSONANT|DIPTHONG|NOT_FIRST,	joint_weight(CONSONANT|DIPTHONG|NOT_FIRST, CONSONANT, CONSONANT|DIPTHONG) * 0.625 * 0.25, s_after_consonant, True),
+	[	Possibility(NUMBER, 0.375, s_first), ],
+	[
+		Possibility(CONSONANT,				joint_weight(CONSONANT, CONSONANT|DIPTHONG, CONSONANT|DIPTHONG|NOT_FIRST) * 0.625 * 0.75, s_after_consonant),
+		Possibility(CONSONANT|DIPTHONG,			joint_weight(CONSONANT|DIPTHONG, CONSONANT, CONSONANT|DIPTHONG|NOT_FIRST) * 0.625 * 0.75, s_after_consonant),
+		Possibility(CONSONANT|DIPTHONG|NOT_FIRST,	joint_weight(CONSONANT|DIPTHONG|NOT_FIRST, CONSONANT, CONSONANT|DIPTHONG) * 0.625 * 0.75, s_after_consonant),
+	],
+	[
+		Possibility(CONSONANT,				joint_weight(CONSONANT, CONSONANT|DIPTHONG, CONSONANT|DIPTHONG|NOT_FIRST) * 0.625 * 0.25, s_after_consonant, True),
+		Possibility(CONSONANT|DIPTHONG,			joint_weight(CONSONANT|DIPTHONG, CONSONANT, CONSONANT|DIPTHONG|NOT_FIRST) * 0.625 * 0.25, s_after_consonant, True),
+		Possibility(CONSONANT|DIPTHONG|NOT_FIRST,	joint_weight(CONSONANT|DIPTHONG|NOT_FIRST, CONSONANT, CONSONANT|DIPTHONG) * 0.625 * 0.25, s_after_consonant, True),
+	],
 ]
 
+## Check that no easily-spotted mistakes were made in probabilities
 for s in (s_first, s_after_consonant, s_after_vowel, s_after_double_vowel):
-	assert(str(numpy.array([p.weight for p in s.possibilities], dtype=numpy.float128).sum()) == "1.0")
+	total_weight = numpy.array([p.weight for pg in s.possibilities for p in pg], dtype=numpy.float128).sum()
+	# print(s.__name__ + ": " + str(total_weight), file=sys.stderr)
+	assert(str(total_weight) == "1.0")
 	
-if __name__ == "__main__":
-	import sys
-
+def generate_all():
 	total = sum(s_first().combinations())
 	pct = total // 100
 
 	print("Generating {0:d} phonemes".format(total), file=sys.stderr)
 
-	maxProb = numpy.float128(0.0)
 	count = 0
-	for (x, probability) in iter(s_first()):
+	for result in s_first().generate():
 		count += 1
-		if probability >= maxProb:
-			print(x + "\t" + str(numpy.uint64((1.0/probability).round())))
-			maxProb = probability
+		print(result.password + "\t" + str(numpy.uint64(numpy.reciprocal(result.probability).round())))
 		if count % pct == 0:
 			print("Generated {0:d} ({1:d}%)...".format(count, count // pct), file=sys.stderr)
 	print("Completed, total={0:d}...".format(count), file=sys.stderr)
+
+if __name__ == "__main__":
+	generate_all()
 
